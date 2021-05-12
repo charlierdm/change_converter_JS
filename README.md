92049ca184bb253c4e7312753d6d3066bb59d0a2
charlierdm <charlie.davismiller@gmail.com>
2021-05-12 18:21:53 +0100

    Change converter functioning, all tests passing

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # change_converter_JS
+
+This is my solution to the Makers Academy process workshop - Change Converter - written in JavaScript.
+
+![coin-shortage-1595446778 (1)](https://user-images.githubusercontent.com/75075773/118017162-2f75c380-b34e-11eb-8a5b-ebf721dda08b.jpg)
+
+## Task
+
+Given a number, convert it into an array representing the most efficient way to break down that amount into pounds.
+
+## Acceptance Critera
+
+```
+change_generator.convert(7.42)
+=> ["£5", "£1", "£1", "20p", "20p", "2p"]
+change_generator.convert(40)
+=> ["£20", "£20"]
+change_generator.convert(16.30)
+=> ["£10", "£5", "£1", "20p", "10p"]
+change_generator.convert(19.99)
+=> ["£10", "£5", "£1", "£1", "£1", "£1", "50p", "20p", "20p", "5p", "2p", "2p"]
+change_generator.convert(76.81)
+=> ["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"]
+```
---
+++ spec/ChangeConverterSpec.js
@@ -0,0 +1,38 @@
+describe('changeConverter', () => {
+
+  it('converts £7.42', () => {
+    expect(changeConverter(7.42)).toEqual(["£5", "£1", "£1", "20p", "20p", "2p"])
+  })
+
+  it('converts £40', () => {
+    expect(changeConverter(40)).toEqual(["£20", "£20"])
+  })
+
+  it('converts £16.30', () => {
+    expect(changeConverter(16.30)).toEqual(["£10", "£5", "£1", "20p", "10p"])
+  })
+
+  it('converts £19.99', () => {
+    expect(changeConverter(19.99)).toEqual(["£10", "£5", "£1", "£1", "£1", "£1", "50p", "20p", "20p", "5p", "2p", "2p"])
+  })
+
+  it('converts £76.81', () => {
+    expect(changeConverter(76.81)).toEqual(["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"])
+  })
+})
+
+
+/*
+
+change_generator.convert(7.42)
+ => ["£5", "£1", "£1", "20p", "20p", "2p"]
+change_generator.convert(40)
+ => ["£20", "£20"]
+change_generator.convert(16.30)
+ => ["£10", "£5", "£1", "20p", "10p"]
+change_generator.convert(19.99)
+ => ["£10", "£5", "£1", "£1", "£1", "£1", "50p", "20p", "20p", "5p", "2p", "2p"]
+change_generator.convert(76.81)
+ => ["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"]
+
+*/

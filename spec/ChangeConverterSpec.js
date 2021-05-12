9f73d821bfaa4ac533ecc305df8109b601224b0c
Charlie Davis-Miller <75075773+charlierdm@users.noreply.github.com>
2021-05-12 19:32:13 +0100

    Update ChangeConverterSpec.js

--- spec/ChangeConverterSpec.js
+++ spec/ChangeConverterSpec.js
@@ -20,19 +20,3 @@ describe('changeConverter', () => {
     expect(changeConverter(76.81)).toEqual(["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"])
   })
 })
-
-
-/*
-
-change_generator.convert(7.42)
- => ["£5", "£1", "£1", "20p", "20p", "2p"]
-change_generator.convert(40)
- => ["£20", "£20"]
-change_generator.convert(16.30)
- => ["£10", "£5", "£1", "20p", "10p"]
-change_generator.convert(19.99)
- => ["£10", "£5", "£1", "£1", "£1", "£1", "50p", "20p", "20p", "5p", "2p", "2p"]
-change_generator.convert(76.81)
- => ["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"]
-
-*/
